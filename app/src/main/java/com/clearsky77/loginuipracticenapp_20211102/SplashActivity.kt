13846094f17403ed3799614a76f7fd35de4e3e58
clearsky77 <clearsky3264@gmail.com>
2021-11-04 17:54:52 +0900

    2.5초 띄우고 로그인 페이지로 전환

--- app/src/main/java/com/clearsky77/loginuipracticenapp_20211102/SplashActivity.kt
+++ app/src/main/java/com/clearsky77/loginuipracticenapp_20211102/SplashActivity.kt
@@ -1,13 +1,22 @@
 package com.clearsky77.loginuipracticenapp_20211102
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({ //Runnable
+            val myIntent = Intent(this, MainActivity :: class.java)
+            startActivity(myIntent)
+        },2500)
 
     }
 }
