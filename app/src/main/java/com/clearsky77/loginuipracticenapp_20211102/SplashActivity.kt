268ccd22a85ab1f4d5b6919717828eba750aaa14
clearsky77 <clearsky3264@gmail.com>
2021-11-04 17:57:51 +0900

    intent 화면에서 back 버튼 누르면 splash 화면 보이는 것 해결 - 인텐트는 얹어 준 것이기 때문에   splash를 finish() 하지 않으면 밑에 살아있다.

--- app/src/main/java/com/clearsky77/loginuipracticenapp_20211102/SplashActivity.kt
+++ app/src/main/java/com/clearsky77/loginuipracticenapp_20211102/SplashActivity.kt
@@ -15,7 +15,9 @@ class SplashActivity : AppCompatActivity() {
 
         myHandler.postDelayed({ //Runnable
             val myIntent = Intent(this, MainActivity :: class.java)
-            startActivity(myIntent)
+            startActivity(myIntent) // 인텐트로 얹어주고
+            finish() // 이 splash는 없앤다
+            // 그렇지 않으면 뒤로 버튼 누르면 나타난다.
         },2500)
 
     }
