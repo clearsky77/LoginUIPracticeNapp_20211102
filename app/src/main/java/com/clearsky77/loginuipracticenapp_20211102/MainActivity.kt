6dccc215d9cefea9d0b0aa5ab3137dd408dc91da
clearsky77 <clearsky3264@gmail.com>
2021-11-04 18:18:36 +0900

    로그인 구현 2. Id와 password로 로그인 성공 실패 여부

--- app/src/main/java/com/clearsky77/loginuipracticenapp_20211102/MainActivity.kt
+++ app/src/main/java/com/clearsky77/loginuipracticenapp_20211102/MainActivity.kt
@@ -2,10 +2,23 @@ package com.clearsky77.loginuipracticenapp_20211102
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+            val txtId = txtId.text.toString()
+            val txtPw = txtPassword.text.toString()
+
+            if(txtId == "admin" && txtPw == "qwer") {
+                Toast.makeText(this,"로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
